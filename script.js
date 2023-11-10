05c1e1cfa23333fb4ca150a5a17b6746c1af33a1
Alex <131786787+Spelks@users.noreply.github.com>
2023-11-10 13:52:09 +0000

    Add class toggles to change dark mode button.

--- script.js
+++ script.js
@@ -5,10 +5,15 @@ const setTheme = document.querySelector("body");
 const themeButton = document.querySelector(".theme-toggle");
 const mainBtn = document.querySelectorAll(".btn");
 const calcWindow = document.querySelector(".calc-window");
+const toggleMoon = document.querySelector(".calc-moon");
+const toggleSun = document.querySelector(".calc-sun");
 themeButton.addEventListener("click", changeTheme);
 calcWindow.textContent = "1234"
 
 function changeTheme() {
+    themeButton.classList.toggle("theme-toggle-light");
+    toggleMoon.classList.toggle("calc-moon-hidden");
+    toggleSun.classList.toggle("calc-sun");
     setTheme.classList.toggle("dark-background");
     mainBtn.forEach(item => item.classList.toggle("dark-btns"));
     calcWindow.classList.toggle("dark-window");
