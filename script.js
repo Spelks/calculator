1d22557851ba67ff681073878162ecae78ed2762
Alex <131786787+Spelks@users.noreply.github.com>
2023-11-10 11:28:09 +0000

    Add function for dark mode transition.

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 const currentDate = document.querySelector(".current-date");
 currentDate.textContent = new Date().getFullYear();
+
+const setTheme = document.querySelector("body");
+const themeButton = document.querySelector(".theme-toggle");
+const mainBtn = document.querySelectorAll(".btn");
+const calcWindow = document.querySelector(".calc-window");
+themeButton.addEventListener("click", changeTheme);
+calcWindow.textContent = "1234"
+
+function changeTheme() {
+    setTheme.classList.toggle("dark-background");
+    mainBtn.forEach(item => item.classList.toggle("dark-btns"));
+    calcWindow.classList.toggle("dark-window");
+}
